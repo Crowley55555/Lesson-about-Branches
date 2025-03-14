7649ec09a8a63e85e39f505bdee0d9cf7db8656c
Crowley55555 <lavrovartem0511@gmail.com>
2025-03-14 19:08:50 +0300

    add a code to brunch main

---
+++ main.py
@@ -0,0 +1,8 @@
+sport = input("Enter a sport: ")
+p1_score = int(input("Enter player 1's score: "))
+p2_score = int(input("Enter player 2's score: "))
+
+if p1_score > p2_score:
+    print("Player 1 wins!")
+else:
+    print("Player 2 wins!")
